e82072d41adcf2edabdcde0e606d4e25ff852be6
Juan Diego Tovaria <juken.555@hotmail.com>
2020-07-02 10:57:00 -0500

    Login firebase funcional sin llaves de autorizacion ni servicio auth

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  usuario: UsuarioModel = new UsuarioModel();
+
+  constructor( private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login(form: NgForm) {
+    if (form.invalid) {return;}
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor'
+    });
+    Swal.showLoading();
+
+    this.auth.login( this.usuario).subscribe( resp => {
+      console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
+    }, (err) =>{
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+      console.log(err.error.error.message)
+    });
+  }
+
+}
---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from "@angular/forms";
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel = new UsuarioModel();
+
+  constructor( private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+
+   }
+
+   onSubmit( form: NgForm ){
+
+    if( form.invalid){ return;}
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor'
+    });
+    Swal.showLoading();
+
+    this.auth.nuevoUsuario(this.usuario).subscribe( resp => {
+      console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+      console.log(err.error.error.message);
+    });
+   
+   }
+
+}
